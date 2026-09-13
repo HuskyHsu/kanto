6a7543d50eca031ef1bcbdb53648034797d83865
HuskyHsu <schi.hsu@gmail.com>
2026-09-13 15:59:45 +0800

    feat: replace CSS loading animation with pixelated PokemonBall image

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -7,7 +7,13 @@ interface LoadingProps {
 export function Loading({ size = 'h-[50vh]' }: LoadingProps) {
   return (
     <div className={`flex ${size} w-full items-center justify-center`}>
-      <div className='wobbling relative' />
+      <div className='wobbling relative flex items-center justify-center'>
+        <img
+          src={`${import.meta.env.BASE_URL}images/type/PokemonBall_.png`}
+          alt='Loading...'
+          className='wobble-ball'
+        />
+      </div>
     </div>
   );
 }
